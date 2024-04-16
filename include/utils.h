39a270f2005808d885988977bca94b543ab741d9
DiegoWearden <diegowearden@utexas.edu>
2024-04-15 22:37:24 -0500

    got a version of printf working

--- include/utils.h
+++ include/utils.h
@@ -4,5 +4,6 @@
 extern "C" void delay ( unsigned long);
 extern "C" void put32 ( unsigned long, unsigned int );
 extern "C" unsigned int get32 ( unsigned long );
+extern "C" int get_el(void);
 
 #endif  /*_BOOT_H */
