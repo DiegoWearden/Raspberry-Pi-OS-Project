265aceda4775a8a93ddc330c09ab4fe3ee3f6f4f
Alexander <alogx@LAPTOP-EV3CO610>
2024-04-16 16:59:17 -0500

    trying to boot multiple cores

--- src/kernel.cc
+++ src/kernel.cc
@@ -3,21 +3,24 @@
 #include "printf.h"
 #include "utils.h"
 
-void putc(void *p, char c){
-    if(c == '\n'){
+void putc(void *p, char c)
+{
+    if (c == '\n')
+    {
         uart_send('\r');
     }
     uart_send(c);
-
 }
 
-extern "C" void kernel_main(void) {
+extern "C" void kernel_main(void)
+{
     uart_init();
     init_printf(0, putc);
     printf("Hello world: %d\r\n", 3);
     printf("Exception level: %d\n", get_el());
-
-    while (1) {
+    printf("processor ID: %d\n", get_core_number());
+    while (1)
+    {
         uart_send(uart_recv());
     }
 }
