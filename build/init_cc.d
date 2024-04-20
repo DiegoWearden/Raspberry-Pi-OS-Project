237448e845457e92757d2fb2eec9280ee6f38f38
DiegoWearden <diegowearden@utexas.edu>
2024-04-20 15:01:22 -0500

    four cores running

---
+++ build/init_cc.d
@@ -0,0 +1,3 @@
+build/init_cc.o: src/init.cc include/init.h include/mini_uart.h \
+ include/printf.h include/utils.h include/stdint.h include/critical.h \
+ include/atomic.h include/utils.h include/printf.h include/mm.h
---
+++ build/printf_cc.d
@@ -0,0 +1,1 @@
+build/printf_cc.o: src/printf.cc include/printf.h include/mini_uart.h
