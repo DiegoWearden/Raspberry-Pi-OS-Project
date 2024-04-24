90d4213e42f8dd13f59da51db994b14f74b39fd9
DiegoWearden <diegowearden@utexas.edu>
2024-04-24 13:29:56 -0500

    creating threads on heap

--- build/heap_cc.d
+++
@@ -1,3 +0,0 @@
-build/heap_cc.o: src/heap.cc include/heap.h include/stdint.h \
- include/printf.h include/stdint.h include/printf.h include/atomic.h \
- include/utils.h
--- build/printf_cc.d
+++
@@ -1,1 +0,0 @@
-build/printf_cc.o: src/printf.cc include/printf.h include/mini_uart.h
